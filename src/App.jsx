3a2abcc10aea3bf695a23f02fbb4b262bfc1f1fc
itochs <chhu20042@g.nihon-u.ac.jp>
2022-08-05 04:29:26 +0900

    add tooltip like. not move circle

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,78 @@ import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import api from "./api";
 import Axis from "./Axis";
+import { text } from "d3";
 
-function ZoomableSVG({ children, width, height }) {
-  console.log("ZoomableSVG");
-  const [k, setK] = useState(1);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+function ZoomableSVG({ children, width, height, xScale, line, pathRef }) {
+  // console.log("ZoomableSVG");
   const svgRef = useRef();
-  useEffect(() => {
-    const zoom = d3
-      .zoom()
-      .scaleExtent([0.5, 3])
-      .translateExtent([
-        [0, 0],
-        [width, height],
-      ])
-      .on("zoom", (event) => {
-        const { x, y, k } = event.transform;
-        setK(k);
-        setX(x);
-        setY(y);
-      });
-    d3.select(svgRef.current).call(zoom);
-  }, []);
+  // const extent = [
+  //   [0, 0],
+  //   [width, height],
+  // ];
+  // useEffect(() => {
+  //   const zoom = d3
+  //     .zoom()
+  //     .scaleExtent([1, 32])
+  //     .extent([
+  //       [0, 0],
+  //       [500, 500],
+  //     ])
+  //     .translateExtent([
+  //       [0, -100],
+  //       [500, -100],
+  //     ])
+  //     // .translateExtent(extent)
+  //     // .extent(extent)
+  //     .on("zoom", (event) => {
+  //       // extent = d3.event.selection;
+  //       // console.log(extent);
+  //       const { x, y, k } = event.transform;
+  //       xScale
+  //         .domain(event.transform.rescaleX(xScale).domain())
+  //         .range([0, 400].map((d) => event.transform.applyX(d)));
+  //       // d3.select(pathRef.current).attr("d", line);
+  //     });
+  //   d3.select(svgRef.current).call(zoom);
+
+  //   // const brush = d3
+  //   //   .brushX()
+  //   //   .extent([
+  //   //     [0, 0],
+  //   //     [400, 400],
+  //   //   ])
+  //   //   .on("end", (event) => {
+  //   //     extent = event.selection;
+  //   //     if (extent) {
+  //   //       xScale.domain([xScale.invert(extent[0]), xinvert(extent[1])]);
+  //   //       d3.select(svgRef.current).call(brush.move, null);
+  //   //     }
+  //   //   });
+  //   // d3.select(svgRef.current).call(brush);
+  // }, []);
   return (
     <svg ref={svgRef} viewBox={`0 0 ${width + 100} ${height + 100}`}>
-      <g transform={`translate(${x}, 0) scale(${k})`}>{children}</g>
+      <g>{children}</g>
     </svg>
   );
 }
 
-function TrendChartContent({ widht, height, runData, yearData }) {
+function TrendChartContent({ line, lineItem, xTicks, yTicks }) {
+  console.log("ChartContent");
+  return (
+    <g>
+      <Axis {...{ xTicks, yTicks }} />
+    </g>
+  );
+}
+
+function TrendChart(props) {
+  // console.log(props);
+  // console.log(props["data"]);
+  const yearData = props["yearData"];
+  const runData = props["data"];
+  // console.log(yearData);
+
   const xScale = d3
     .scaleLinear()
     .domain([0, (yearData.length - 1) * 12])
@@ -64,7 +105,8 @@ function TrendChartContent({ widht, height, runData, yearData }) {
     })
     .flat();
   // console.log("line item");
-  // console.log(lineItem);
+  console.log("line item");
+  console.log(lineItem);
   const line = d3
     .line()
     .x((d) => d.x)
@@ -82,60 +124,58 @@ function TrendChartContent({ widht, height, runData, yearData }) {
       label: y,
     };
   });
-  console.log("ChartContent");
-  return (
-    <g>
-      <g transform="translate(50, 50)">
-        <path
-          d={line(lineItem)}
-          fill={"none"}
-          stroke={"black"}
-          strokeWidth={"3"}
-        />
-      </g>
-      <Axis {...{ xTicks, yTicks }} />
-    </g>
-  );
-}
 
-function TrendChart(props) {
-  // console.log(props);
-  // console.log(props["data"]);
-  const yearData = props["yearData"];
-  const runData = props["data"];
-  // console.log(yearData);
-
+  const pathRef = useRef();
   // return <svg id="chart" ref={svgRef} viewBox="0 0 500 500"></svg>;
   return (
-    <ZoomableSVG width={400} height={400}>
-      <TrendChartContent
-        width={400}
-        height={400}
-        runData={runData}
-        yearData={yearData}
-      />
+    <ZoomableSVG
+      width={400}
+      height={400}
+      xScale={xScale}
+      line={line}
+      pathRef={pathRef}
+    >
+      <TrendChartContent {...{ line, lineItem, xTicks, yTicks }} />
+      <g transform="translate(50, 50)">
+        <circle x={400} y={200} r={20} stroke={"red"} fill={"red"} />
+        <g>
+          <path
+            ref={pathRef}
+            className="path"
+            d={line(lineItem)}
+            fill={"none"}
+            stroke={"black"}
+            strokeWidth={"3"}
+          />
+        </g>
+        <g>
+          {lineItem.map((item, index) => {
+            let over = false;
+            return (
+              <g key={index}>
+                <title>{item.x}</title>
+                <rect
+                  key={index}
+                  x={item.x}
+                  y={0}
+                  width={4}
+                  height={400}
+                  fill={"red"}
+                  opacity={0}
+                  onMouseOver={(event) => {
+                    over = true;
+                  }}
+                />
+                <circle x={item.x} y={0} r={5} stroke={"red"} fill={"red"} />
+              </g>
+            );
+          })}
+        </g>
+        <circle x={100} y={0} r={20} stroke={"red"} fill={"red"} />
+        <Axis {...{ xTicks, yTicks }} />
+      </g>
     </ZoomableSVG>
   );
-}
-
-function StackChart() {
-  <svg viewBox="0 0 600 600">
-    <g transform="translate(10 0)">
-      {data.map((item, i) => {
-        // console.log(yScale(i));
-        return (
-          <rect
-            key={i}
-            x={xScale(i)}
-            y={yScale(item["len"])}
-            width={50}
-            height={graphHeight - yScale(item["len"])}
-            fill="pink"
-          />
-        );
-      })}
-    </g>
-  </svg>;
 }
 
 function App() {
@@ -156,41 +196,6 @@ function App() {
   const graphHeight = 600;
   console.log("api");
   console.log(data);
-  // const runData = parseData(data);
-  // console.log(JSON.stringify(runData, null, 1));
-  // const xScale = d3
-  //   .scaleLinear()
-  //   .domain([0, data.length])
-  //   .range([0, graphWidth])
-  //   .nice();
-  // const yScale = d3
-  //   .scaleLinear()
-  //   .domain(d3.extent(data, (item) => item["len"]))
-  //   .range([graphHeight, 0])
-  //   .nice();
-
-  /*
-      To see how stack.value can be used, consider a dataset formatted as follows:
-  
-  var data = [
-    {month: new Date(2018, 1, 1), fruitSales: {apples: 10, bananas: 20, oranges: 15}},
-    ...
-  To create a stack generator with this dataset we call d3.stack and then set the keys to the names of the fruits as we did in the previous examples. We then call stack.value, passing it a lambda expression. The lambda expression has two parameters, obj and key, which correspond to an object in data and a key passed to stack.keys, respectively, and returns the value associated with the key in the object obj.
-  
-  var stack = d3.stack()
-    .keys(["apples", "bananas", "oranges"])
-    .value((obj, key) => obj.fruitSales[key]); 
-      */
-  //   const subgroups = Object.keys(data[0]["month"]);
-  //   console.log("subgrounp");
-  //   console.log(subgroups);
-  //   // const stackedData = d3.stack().keys(subgroups)(data)
-  //   const stackedData = d3.stack().keys(subgroups)(
-  //     data.map((item) => item["month"])
-  //   );
-  //   // .value((data, key) => data["month"][key]);
-  //   console.log("stack");
-  //   console.log(stackedData);
 
   const { rundata, players, yearData } = data;
   // console.log(data["yearData"]);
--- src/Axis.jsx
+++ src/Axis.jsx
@@ -1,10 +1,10 @@
 function XAxis({ xTicks }) {
-  <g className="x-axis" transform="translate(50, -50)">
-    <line x1={0} y1={400} x2={400} y2={400} stroke={"black"} strokeWidth={1} />
+  <g className="x-axis" transform="translate(50, 400)">
+    <line x1={0} y1={0} x2={400} y2={0} stroke={"black"} strokeWidth={1} />
     <g className="x-axis-label">
       {xTicks.map((tick, index) => {
         return (
-          <g key={index} transform={`translate(${tick.x}, 500)`}>
+          <g key={index} transform={`translate(${tick.x}, 0)`}>
             <line x1={0} y1={0} x2={0} y2={5} stroke={"black"} />
             <text
               x={0}
@@ -25,7 +25,7 @@ function XAxis({ xTicks }) {
 
 function YAxis({ yTicks }) {
   return (
-    <g className="y-axis" transform="translate(50, 50)">
+    <g className="y-axis" transform="translate(0, 0)">
       <line x1={0} y1={0} x2={0} y2={400} stroke={"black"} strokeWidth={1} />
       {yTicks.map((tick, index) => {
         return (
