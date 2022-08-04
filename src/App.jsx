111d9b3a9b5b08e9b179f4bd8d45a7e69b98943b
itochs <chhu20042@g.nihon-u.ac.jp>
2022-08-05 02:28:22 +0900

    add axis file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import api from "./api";
+import Axis from "./Axis";
 
 function ZoomableSVG({ children, width, height }) {
   console.log("ZoomableSVG");
@@ -92,58 +93,7 @@ function TrendChartContent({ widht, height, runData, yearData }) {
           strokeWidth={"3"}
         />
       </g>
-      <XAxis {...{ xTicks }} />
-      <YAxis {...{ yTicks }} />
-    </g>
-  );
-}
-
-function XAxis({ xTicks }) {
-  <g className="x-axis" transform="translate(50, -50)">
-    <line x1={0} y1={400} x2={400} y2={400} stroke={"black"} strokeWidth={1} />
-    <g className="x-axis-label">
-      {xTicks.map((tick, index) => {
-        return (
-          <g key={index} transform={`translate(${tick.x}, 500)`}>
-            <line x1={0} y1={0} x2={0} y2={5} stroke={"black"} />
-            <text
-              x={0}
-              y={10}
-              fontSize={12}
-              textAnchor={"middle"}
-              dominantBaseline={"hanging"}
-              style={{ userSelect: "none" }}
-            >
-              {tick.label}
-            </text>
-          </g>
-        );
-      })}
-    </g>
-  </g>;
-}
-
-function YAxis({ yTicks }) {
-  return (
-    <g className="y-axis" transform="translate(50, 50)">
-      <line x1={0} y1={0} x2={0} y2={400} stroke={"black"} strokeWidth={1} />
-      {yTicks.map((tick, index) => {
-        return (
-          <g key={index} transform={`translate(0, ${tick.y})`}>
-            <line x1={0} y1={0} x2={-5} y2={0} stroke={"black"} />
-            <text
-              x={-10}
-              y={0}
-              fontSize={12}
-              textAnchor={"end"}
-              dominantBaseline={"middle"}
-              style={{ userSelect: "none" }}
-            >
-              {tick.label}
-            </text>
-          </g>
-        );
-      })}
+      <Axis {...{ xTicks, yTicks }} />
     </g>
   );
 }
---
+++ src/Axis.jsx
@@ -0,0 +1,58 @@
+function XAxis({ xTicks }) {
+  <g className="x-axis" transform="translate(50, -50)">
+    <line x1={0} y1={400} x2={400} y2={400} stroke={"black"} strokeWidth={1} />
+    <g className="x-axis-label">
+      {xTicks.map((tick, index) => {
+        return (
+          <g key={index} transform={`translate(${tick.x}, 500)`}>
+            <line x1={0} y1={0} x2={0} y2={5} stroke={"black"} />
+            <text
+              x={0}
+              y={10}
+              fontSize={12}
+              textAnchor={"middle"}
+              dominantBaseline={"hanging"}
+              style={{ userSelect: "none" }}
+            >
+              {tick.label}
+            </text>
+          </g>
+        );
+      })}
+    </g>
+  </g>;
+}
+
+function YAxis({ yTicks }) {
+  return (
+    <g className="y-axis" transform="translate(50, 50)">
+      <line x1={0} y1={0} x2={0} y2={400} stroke={"black"} strokeWidth={1} />
+      {yTicks.map((tick, index) => {
+        return (
+          <g key={index} transform={`translate(0, ${tick.y})`}>
+            <line x1={0} y1={0} x2={-5} y2={0} stroke={"black"} />
+            <text
+              x={-10}
+              y={0}
+              fontSize={12}
+              textAnchor={"end"}
+              dominantBaseline={"middle"}
+              style={{ userSelect: "none" }}
+            >
+              {tick.label}
+            </text>
+          </g>
+        );
+      })}
+    </g>
+  );
+}
+
+export default function Axis({ xTicks, yTicks }) {
+  return (
+    <g>
+      <XAxis {...{ xTicks }} />
+      <YAxis {...{ yTicks }} />
+    </g>
+  );
+}
